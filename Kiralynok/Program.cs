1d44c3e8115382b6fe0ffc8d39d4e1db17893623
Medve Adrián <medveadrian.szgya@gmail.com>
2022-12-06 14:32:20 +0100

    Add project files.

---
+++ Kiralynok/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Kiralynok
+{
+    class Program
+    {
+        static Tabla tabla;
+        static void Main(string[] args)
+        {
+            NegyedikFeladat();
+            HatodikFeladat();
+            KilencedikFeladat();
+            TizedikFeladat();
+
+            Console.ReadKey();
+        }
+
+        private static void TizedikFeladat()
+        {
+            if (File.Exists("tablak64.txt"))
+            {
+                File.Delete("tablak64.txt");
+            }
+
+
+            using (StreamWriter sw = new StreamWriter("tablak64.txt"))
+            {
+                for (int i = 1; i <= 64; i++)
+                {
+                    Tabla newTabla = new Tabla('*');
+                    newTabla.Elhelyez(i);
+                    sw.WriteLine(newTabla.Megjelenit());
+                }
+            }
+        }
+
+        private static void KilencedikFeladat()
+        {
+            Console.WriteLine("\n9. feladat: Üres oszlopok és sorok száma:");
+            Console.WriteLine("Oszlopok: " + tabla.UresOszlopokSzama);
+            Console.WriteLine("Sorok: " + tabla.UresSorokSzama);
+        }
+
+        private static void HatodikFeladat()
+        {
+            Console.WriteLine("\n5. feladat: A feltöltött tábla:");
+            tabla.Elhelyez(8);
+            Console.WriteLine(tabla.Megjelenit());
+        }
+
+        private static void NegyedikFeladat()
+        {
+            Console.WriteLine("4. feladat: Az üres tábla:");
+            tabla = new Tabla('#');
+            Console.WriteLine(tabla.Megjelenit());
+        }
+    }
+}
---
+++ Kiralynok/Tabla.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralynok
+{
+    class Tabla
+    {
+        private char[,] T;
+
+        private char UresCella;
+
+        public int UresOszlopokSzama
+        {
+            get
+            {
+                int oszlopok = 0;
+                for (int oszlop = 0; oszlop < 8; oszlop++)
+                {
+                    if (UresOszlop(oszlop))
+                    {
+                        oszlopok++;
+                    }
+                }
+                return oszlopok;
+            }
+        }
+
+        public int UresSorokSzama
+        {
+            get
+            {
+                int sorok = 0;
+                for (int sor = 0; sor < 8; sor++)
+                {
+                    if (UresSor(sor))
+                    {
+                        sorok++;
+                    }
+                }
+                return sorok;
+            }
+        }
+
+        public Tabla(char uresCella)
+        {
+            T = new char[8,8];
+            UresCella = uresCella;
+
+            for (int sor = 0; sor < 8; sor++)
+            {
+                for (int oszlop = 0; oszlop < 8; oszlop++)
+                {
+                    T[sor, oszlop] = UresCella;
+                }
+            }
+        }
+
+        public string Megjelenit()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int sor = 0; sor < 8; sor++)
+            {
+                for (int oszlop = 0; oszlop < 8; oszlop++)
+                {
+                    sb.Append(T[sor, oszlop]);
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+
+        public void Elhelyez(int N)
+        {
+            Random rnd = new Random();
+            while (N > 0)
+            {
+                int sor = rnd.Next(8);
+                int oszlop = rnd.Next(8);
+                if (T[sor, oszlop] == UresCella)
+                {
+                    T[sor, oszlop] = 'K';
+                    N--;
+                }
+            }
+
+        }
+
+        public bool UresOszlop(int oszlop)
+        {
+            for (int sor = 0; sor < 8; sor++)
+            {
+                if (T[sor, oszlop] == 'K')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool UresSor(int sor)
+        {
+            for (int oszlop = 0; oszlop < 8; oszlop++)
+            {
+                if (T[sor, oszlop] == 'K')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
